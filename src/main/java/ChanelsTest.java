286ca6678781e3baf776d9e5d9d394789fabec80
cecsergey <cecsergey@gmail.com>
2016-04-03 22:49:19 +0400

    Ucom channels test

---
+++ src/main/java/ChanelsTest.java
@@ -0,0 +1,128 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Arrays;
+
+/**
+ * Created by cecse on 4/2/2016.
+ */
+public class ChanelsTest {
+    private static String temp,temp2;
+    private static String[] channels  = {"1TV", "Հ2", "Շանթ", "Արմենիա TV", "Կենտրոն", "Արարատ",
+                                            "Երկիր Մեդիա", "ATV", "ԱՐ", "21 TV", "ԱրմՆյուզ",
+                                        "Հ3", "Շողակաթ", "Россия 1", "Первый канал", "РТР Планетa",
+                                        "Россия К (Культура)", "HTB", "Перец ТВ", "TNT", "CTC International",
+                                        "2x2", "Пятый канал", "Домашний Intenational", "РЕН ТВ", "Мир",
+                                        "TV5 Monde", "Telesur", "U! Premium Armenia"};
+    public static void main(String[] args){
+
+        WebDriver driver = new FirefoxDriver();
+
+
+        //navigate to ucom.am site
+        driver.get("https://www.ucom.am/en/home.html");
+        driver.manage().window().maximize();
+
+        //find and click services tab
+        WebElement services = driver.findElement(By.xpath("//*[@id=\"j_id25\"]/div[3]/div[1]/a/span[1]"));
+        services.click();
+
+        //find and click on epg link
+        WebElement epg = driver.findElement(By.xpath("//*[@id=\"j_id25\"]/div[3]/div[2]/ul/li[2]/ul/li[2]/ul/li[5]/a"));
+        epg.click();
+
+        //find and click on publicChannels link
+        WebElement publicChanels = driver.findElement(By.xpath("//*[@id=\"j_id174:j_id176:0:tvCategoryId_\"]"));
+        publicChanels.click();
+
+
+        //compare existing WebElement list with channels list
+        temp = driver.findElement(EPGScreen.epgChannel1TV).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannel2x2).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannel21TV).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelAR).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelArarat).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelArmeniaTV).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelArmNews).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelATV).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelDomashniiInternational).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelErkirMedia).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelH2).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelH3).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelKentron).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelMIR).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelNTV).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelPerviiCanal).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelPyatiiCanal).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelRenTV).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelRossia1).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelRossiaK).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelRTRPlaneta).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelShant).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelShoghakat).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelSTSInternational).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelTelesur).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelTNT).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelTV5Monde).getText();
+        contains(channels,temp);
+        temp = driver.findElement(EPGScreen.epgChannelUPremium).getText();
+        contains(channels,temp);
+
+
+        //Get list of public channels from site
+        temp2 = driver.findElement(By.xpath("//*[@id=\"channelList\"]/div[2]/div[1]")).getText();
+        String[] publicChannels = temp2.split("\n");
+
+
+        //Compare web page channels list with existing channels list
+        for(int i=0;i<channels.length;i++){
+            if(Arrays.asList(publicChannels).contains(channels[i])){
+
+            }else{
+                System.out.println("The public channels page doesn't contain  "+ channels[i]+" channel");
+            }
+        }
+
+        driver.close();
+        driver.quit();
+
+    }
+
+    public static void contains(String[] channelsArray, String channel) {
+        if(Arrays.asList(channelsArray).contains(channel)){
+            //System.out.println("The channel "+ channel+" exist");
+        }else{
+            System.out.println("The channel "+ channel+" doesn't exist");
+        }
+
+    }
+
+}
---
+++ src/main/java/EPGScreen.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+
+/**
+ * Created by cecse on 4/3/2016.
+ */
+public class EPGScreen {
+
+
+    static final By epgChannel1TV = By.id("epgChannelName067");
+    static final By epgChannelH2 = By.id("epgChannelName071");
+    static final By epgChannelShant = By.id("epgChannelName069");
+    static final By epgChannelArmeniaTV = By.id("epgChannelName068");
+    static final By epgChannelKentron = By.id("epgChannelName075");
+    static final By epgChannelArarat = By.id("epgChannelName072");
+    static final By epgChannelErkirMedia = By.id("epgChannelName073");
+    static final By epgChannelATV = By.id("epgChannelName074");
+    static final By epgChannelAR = By.id("epgChannelName070");
+    static final By epgChannel21TV = By.id("epgChannelName076");
+    static final By epgChannelArmNews = By.id("epgChannelName117");
+    static final By epgChannelH3 = By.id("epgChannelName115");
+    static final By epgChannelShoghakat = By.id("epgChannelName116");
+    static final By epgChannelPerviiCanal = By.id("epgChannelName058");
+    static final By epgChannelRossia1 = By.id("epgChannelName064");
+    static final By epgChannelRTRPlaneta = By.id("epgChannelName023");
+    static final By epgChannelSTSInternational = By.id("epgChannelName061");
+    static final By epgChannelRossiaK = By.id("epgChannelName062");
+    static final By epgChannelNTV = By.id("epgChannelName045");
+    static final By epgChannelTNT = By.id("epgChannelName060");
+    static final By epgChannel2x2 = By.id("epgChannelName111");
+    static final By epgChannelPyatiiCanal = By.id("epgChannelName097");
+    static final By epgChannelDomashniiInternational = By.id("epgChannelName098");
+    static final By epgChannelRenTV = By.id("epgChannelName087");
+    static final By epgChannelMIR = By.id("epgChannelName096");
+    static final By epgChannelTV5Monde = By.id("epgChannelName121");
+    static final By epgChannelTelesur = By.id("epgChannelName085");
+    static final By epgChannelUPremium = By.id("epgChannelName000");
+
+
+}
